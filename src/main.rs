5883714f8b81887cce631a0dc41773e8d0179ffa
Yasuaki Gohko <ygohko@gmail.com>
2024-05-04 08:56:34 +0900

    Added metadata checking.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::fs;
 use std::io;
+use std::path::PathBuf;
+
+fn process_file(path: &PathBuf) -> io::Result<()> {
+    let metadata = std::fs::metadata(path)?;
+    if (metadata.is_file()) {
+	println!("This is file.");
+    } else {
+	println!("This is not file.");
+    }
+
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
     println!("Hello, world!");
 
     let result = fs::read_dir(".")?;
     for entry in result.into_iter() {
-	println!("{}", entry?.path().display());
+	let path = entry?.path();
+	println!("{}", path.display());
+	process_file(&path);
     }
 
     Ok(())
