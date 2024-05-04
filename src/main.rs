3cf0ef93fbf8ad77836cde735dcff47e14b47b7c
Yasuaki Gohko <ygohko@gmail.com>
2024-05-04 09:41:14 +0900

    Added test codes to read files.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use std::io;
 use std::path::PathBuf;
 
 fn process_file(path: &PathBuf) -> io::Result<()> {
-    let metadata = std::fs::metadata(path)?;
+    let metadata = fs::metadata(path)?;
     if (metadata.is_file()) {
 	println!("This is file.");
+
+	let values = fs::read(path)?;
+	println!("{} bytes read.", values.len());
+	
+
     } else {
 	println!("This is not file.");
     }
