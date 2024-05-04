6183c911247627e57ce45b2df2e76bd20fd3ae00
Yasuaki Gohko <ygohko@gmail.com>
2024-05-04 15:38:45 +0900

    Generated hash values from files.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::io;
 use std::path::PathBuf;
+use sha1::Digest;
+use sha1::Sha1;
 
 fn process_file(path: &PathBuf) -> io::Result<()> {
     let metadata = fs::metadata(path)?;
@@ -9,6 +11,11 @@ fn process_file(path: &PathBuf) -> io::Result<()> {
 
 	let values = fs::read(path)?;
 	println!("{} bytes read.", values.len());
+	let mut sha1 = Sha1::new();
+	sha1.update(values);
+	let hash = sha1.finalize();
+	let hash_values = hash.to_vec();
+	println!("{} bytes of hash generated.", hash_values.len());
 	
 
     } else {
