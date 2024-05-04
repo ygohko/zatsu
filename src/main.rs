4f1bc80253604903649259b1d1cac610bba10093
Yasuaki Gohko <ygohko@gmail.com>
2024-05-04 16:26:25 +0900

    Shown hash values of files.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use hex_string::HexString;
+use sha1::Digest;
+use sha1::Sha1;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
-use sha1::Digest;
-use sha1::Sha1;
 
 fn process_file(path: &PathBuf) -> io::Result<()> {
     let metadata = fs::metadata(path)?;
@@ -16,7 +17,9 @@ fn process_file(path: &PathBuf) -> io::Result<()> {
 	let hash = sha1.finalize();
 	let hash_values = hash.to_vec();
 	println!("{} bytes of hash generated.", hash_values.len());
-	
+	// println!("{}", hash_values);
+	let hex = HexString::from_bytes(&hash_values);
+	println!("{}", hex.as_string());
 
     } else {
 	println!("This is not file.");
