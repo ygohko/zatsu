1cd543a38ab1845586f3547b5bb8a6e374e37652
Yasuaki Gohko <ygohko@gmail.com>
2024-08-13 07:36:47 +0900

    Added a method to save a file.

--- src/get_command.rs
+++ src/get_command.rs
@@ -38,6 +38,9 @@ pub struct GetCommand {
 
 impl Command for GetCommand {
     fn execute(&self) -> Result<(), ZatsuError> {
+	self.save_file()
+
+	/*
 	let repository = match Repository::load(".zatsu/repository.json") {
 	    Ok(repository) => repository,
 	    Err(_) => Repository {
@@ -99,6 +102,7 @@ impl Command for GetCommand {
 	};
 
 	Ok(())
+	*/
     }	
 }
 
@@ -108,5 +112,69 @@ impl GetCommand {
 	    revision_number,
 	    path: path.to_string(),
 	}
+    }
+
+    fn save_file(&self) -> Result<(), ZatsuError> {
+	let repository = match Repository::load(".zatsu/repository.json") {
+	    Ok(repository) => repository,
+	    Err(_) => Repository {
+		revision_numbers: Vec::new(),
+	    },
+	};
+	let mut found = false;
+	for a_revision_number in repository.revision_numbers {
+	    if a_revision_number == self.revision_number {
+		found = true;
+	    } 
+	}
+	if !found {
+	    return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
+	}
+
+	let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
+	    Ok(revision) => revision,
+	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
+	};
+	let mut hash = "".to_string();
+	let mut found = false;
+	for entry in revision.entries {
+	    if entry.path == *self.path {
+		found = true;
+		hash = entry.hash;
+	    }
+	}
+	if !found {
+	    return Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND));
+	}
+
+	let directory_name = hash[0..2].to_string();
+	let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
+	    Ok(values) => values,
+	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+	};
+	let mut decoder = ZlibDecoder::new(Vec::new());
+	match decoder.write_all(&values) {
+	    Ok(()) => (),
+	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+	};
+	let decoded = match decoder.finish() {
+	    Ok(decoded) => decoded,
+	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+	};
+	let split: Vec<_> = self.path.split("/").collect();
+	let mut file_name = "out.dat".to_string();
+	if split.len() >= 1 {
+	    let original_file_name = split[split.len() - 1].to_string();
+	    let split: Vec<_> = original_file_name.split(".").collect();
+	    if split.len() > 1 {
+		file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
+	    }
+	}
+	match fs::write(&PathBuf::from(file_name), decoded) {
+	    Ok(()) => (),
+	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
+	};
+
+	Ok(())
     }
 }
