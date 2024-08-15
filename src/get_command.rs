3bc397b41cdb88e1f278c96122553c12164d5de8
Yasuaki Gohko <ygohko@gmail.com>
2024-08-15 09:41:06 +0900

    Basic implementatin for saving directory.

--- src/get_command.rs
+++ src/get_command.rs
@@ -38,251 +38,253 @@ pub struct GetCommand {
 
 impl Command for GetCommand {
     fn execute(&self) -> Result<(), ZatsuError> {
-	// TODO: Dicide operation, for a file or a directory.
+        // TODO: Dicide operation, for a file or a directory.
 
-	let repository = match Repository::load(".zatsu/repository.json") {
-	    Ok(repository) => repository,
-	    Err(_) => Repository {
-		revision_numbers: Vec::new(),
-	    },
-	};
-	let mut found = false;
-	for a_revision_number in repository.revision_numbers {
-	    if a_revision_number == self.revision_number {
-		found = true;
-	    } 
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
-	}
+        let repository = match Repository::load(".zatsu/repository.json") {
+            Ok(repository) => repository,
+            Err(_) => Repository {
+                revision_numbers: Vec::new(),
+            },
+        };
+        let mut found = false;
+        for a_revision_number in repository.revision_numbers {
+            if a_revision_number == self.revision_number {
+                found = true;
+            } 
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
+        }
 
-	let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
-	    Ok(revision) => revision,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
-	};
-	let mut hash = "".to_string();
-	let mut file_found = false;
-	let mut directory_found = false;
-	for entry in revision.entries {
-	    if entry.path == *self.path {
-		file_found = true;
-		hash = entry.hash;
-	    }
+        let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
+            Ok(revision) => revision,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
+        };
+        let mut hash = "".to_string();
+        let mut file_found = false;
+        let mut directory_found = false;
+        for entry in revision.entries {
+            if entry.path == *self.path {
+                file_found = true;
+                hash = entry.hash;
+            }
 
-	    if entry.path.contains("/") {
-		if let Some(index) = entry.path.find(*self.path) {
-		    if index < entrypath.len() - 2 {
-			directory_found = true;
-		    }
-		}
-	    }
-	}
+            if entry.path.contains("/") {
+                if let Some(index) = entry.path.find(&self.path) {
+                    if index == 0 && self.path.len() <= entry.path.len() - 2 {
+                        directory_found = true;
+                    }
+                }
+            }
+        }
 
-	if file_found {
-	    return self.save_file();
-	}
-	if directory_found {
-	    return self.save_directory();
-	}
+        if file_found {
+            return self.save_file();
+        }
+        if directory_found {
+            return self.save_directory();
+        }
 
-	Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND))
+        Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND))
 
-	/*
-	let repository = match Repository::load(".zatsu/repository.json") {
-	    Ok(repository) => repository,
-	    Err(_) => Repository {
-		revision_numbers: Vec::new(),
-	    },
-	};
-	let mut found = false;
-	for a_revision_number in repository.revision_numbers {
-	    if a_revision_number == self.revision_number {
-		found = true;
-	    } 
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
-	}
+        /*
+        let repository = match Repository::load(".zatsu/repository.json") {
+            Ok(repository) => repository,
+            Err(_) => Repository {
+                revision_numbers: Vec::new(),
+            },
+        };
+        let mut found = false;
+        for a_revision_number in repository.revision_numbers {
+            if a_revision_number == self.revision_number {
+                found = true;
+            } 
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
+        }
 
-	let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
-	    Ok(revision) => revision,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
-	};
-	let mut hash = "".to_string();
-	let mut found = false;
-	for entry in revision.entries {
-	    if entry.path == *self.path {
-		found = true;
-		hash = entry.hash;
-	    }
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND));
-	}
+        let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
+            Ok(revision) => revision,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
+        };
+        let mut hash = "".to_string();
+        let mut found = false;
+        for entry in revision.entries {
+            if entry.path == *self.path {
+                found = true;
+                hash = entry.hash;
+            }
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND));
+        }
 
-	let directory_name = hash[0..2].to_string();
-	let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
-	    Ok(values) => values,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let mut decoder = ZlibDecoder::new(Vec::new());
-	match decoder.write_all(&values) {
-	    Ok(()) => (),
-	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let decoded = match decoder.finish() {
-	    Ok(decoded) => decoded,
-	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let split: Vec<_> = self.path.split("/").collect();
-	let mut file_name = "out.dat".to_string();
-	if split.len() >= 1 {
-	    let original_file_name = split[split.len() - 1].to_string();
-	    let split: Vec<_> = original_file_name.split(".").collect();
-	    if split.len() > 1 {
-		file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
-	    }
-	}
-	match fs::write(&PathBuf::from(file_name), decoded) {
-	    Ok(()) => (),
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
-	};
+        let directory_name = hash[0..2].to_string();
+        let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
+            Ok(values) => values,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let mut decoder = ZlibDecoder::new(Vec::new());
+        match decoder.write_all(&values) {
+            Ok(()) => (),
+            Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let decoded = match decoder.finish() {
+            Ok(decoded) => decoded,
+            Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let split: Vec<_> = self.path.split("/").collect();
+        let mut file_name = "out.dat".to_string();
+        if split.len() >= 1 {
+            let original_file_name = split[split.len() - 1].to_string();
+            let split: Vec<_> = original_file_name.split(".").collect();
+            if split.len() > 1 {
+                file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
+            }
+        }
+        match fs::write(&PathBuf::from(file_name), decoded) {
+            Ok(()) => (),
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
+        };
 
-	Ok(())
-	*/
-    }	
+        Ok(())
+        */
+    }   
 }
 
 impl GetCommand {
     pub fn new(revision_number: i32, path: &str) -> Self {
-	Self {
-	    revision_number,
-	    path: path.to_string(),
-	}
+        Self {
+            revision_number,
+            path: path.to_string(),
+        }
     }
 
     fn save_file(&self) -> Result<(), ZatsuError> {
-	let repository = match Repository::load(".zatsu/repository.json") {
-	    Ok(repository) => repository,
-	    Err(_) => Repository {
-		revision_numbers: Vec::new(),
-	    },
-	};
-	let mut found = false;
-	for a_revision_number in repository.revision_numbers {
-	    if a_revision_number == self.revision_number {
-		found = true;
-	    } 
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
-	}
+        let repository = match Repository::load(".zatsu/repository.json") {
+            Ok(repository) => repository,
+            Err(_) => Repository {
+                revision_numbers: Vec::new(),
+            },
+        };
+        let mut found = false;
+        for a_revision_number in repository.revision_numbers {
+            if a_revision_number == self.revision_number {
+                found = true;
+            } 
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
+        }
 
-	let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
-	    Ok(revision) => revision,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
-	};
-	let mut hash = "".to_string();
-	let mut found = false;
-	for entry in revision.entries {
-	    if entry.path == *self.path {
-		found = true;
-		hash = entry.hash;
-	    }
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND));
-	}
+        let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
+            Ok(revision) => revision,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
+        };
+        let mut hash = "".to_string();
+        let mut found = false;
+        for entry in revision.entries {
+            if entry.path == *self.path {
+                found = true;
+                hash = entry.hash;
+            }
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_FILE_NOT_FOUND));
+        }
 
-	let directory_name = hash[0..2].to_string();
-	let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
-	    Ok(values) => values,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let mut decoder = ZlibDecoder::new(Vec::new());
-	match decoder.write_all(&values) {
-	    Ok(()) => (),
-	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let decoded = match decoder.finish() {
-	    Ok(decoded) => decoded,
-	    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-	};
-	let split: Vec<_> = self.path.split("/").collect();
-	let mut file_name = "out.dat".to_string();
-	if split.len() >= 1 {
-	    let original_file_name = split[split.len() - 1].to_string();
-	    let split: Vec<_> = original_file_name.split(".").collect();
-	    if split.len() > 1 {
-		file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
-	    }
-	}
-	match fs::write(&PathBuf::from(file_name), decoded) {
-	    Ok(()) => (),
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
-	};
+        let directory_name = hash[0..2].to_string();
+        let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
+            Ok(values) => values,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let mut decoder = ZlibDecoder::new(Vec::new());
+        match decoder.write_all(&values) {
+            Ok(()) => (),
+            Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let decoded = match decoder.finish() {
+            Ok(decoded) => decoded,
+            Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+        };
+        let split: Vec<_> = self.path.split("/").collect();
+        let mut file_name = "out.dat".to_string();
+        if split.len() >= 1 {
+            let original_file_name = split[split.len() - 1].to_string();
+            let split: Vec<_> = original_file_name.split(".").collect();
+            if split.len() > 1 {
+                file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
+            }
+        }
+        match fs::write(&PathBuf::from(file_name), decoded) {
+            Ok(()) => (),
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
+        };
 
-	Ok(())
+        Ok(())
     }
 
     fn save_directory(&self) -> Result<(), ZatsuError> {
-	let repository = match Repository::load(".zatsu/repository.json") {
-	    Ok(repository) => repository,
-	    Err(_) => Repository {
-		revision_numbers: Vec::new(),
-	    },
-	};
-	let mut found = false;
-	for a_revision_number in repository.revision_numbers {
-	    if a_revision_number == self.revision_number {
-		found = true;
-	    } 
-	}
-	if !found {
-	    return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
-	}
+        let repository = match Repository::load(".zatsu/repository.json") {
+            Ok(repository) => repository,
+            Err(_) => Repository {
+                revision_numbers: Vec::new(),
+            },
+        };
+        let mut found = false;
+        for a_revision_number in repository.revision_numbers {
+            if a_revision_number == self.revision_number {
+                found = true;
+            } 
+        }
+        if !found {
+            return Err(ZatsuError::new("main".to_string(), error::CODE_REVISION_NOT_FOUND));
+        }
 
-	let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
-	    Ok(revision) => revision,
-	    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
-	};
-	let mut hash = "".to_string();
-	for entry in revision.entries {
-	    // TODO: Write this entry if it is in this directory.
+        let revision = match Revision::load(format!(".zatsu/revisions/{:02x}/{}.json", self.revision_number & 0xFF, self.revision_number)) {
+            Ok(revision) => revision,
+            Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_REVISION_FAILED)),
+        };
+        let mut hash = "".to_string();
+        for entry in revision.entries {
+            // TODO: Write this entry if it is in this directory.
 
-	    if let Some(index) = entry.path.find(*self.path) {
-		let directory_name = hash[0..2].to_string();
-		let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
-		    Ok(values) => values,
-		    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-		};
-		let mut decoder = ZlibDecoder::new(Vec::new());
-		match decoder.write_all(&values) {
-		    Ok(()) => (),
-		    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-		};
-		let decoded = match decoder.finish() {
-		    Ok(decoded) => decoded,
-		    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
-		};
+            if let Some(index) = entry.path.find(&self.path) {
+                hash = entry.hash;
+                let directory_name = hash[0..2].to_string();
+                let values = match fs::read(&PathBuf::from(format!(".zatsu/objects/{}/{}", directory_name, hash))) {
+                    Ok(values) => values,
+                    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+                };
+                let mut decoder = ZlibDecoder::new(Vec::new());
+                match decoder.write_all(&values) {
+                    Ok(()) => (),
+                    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+                };
+                let decoded = match decoder.finish() {
+                    Ok(decoded) => decoded,
+                    Err(_) =>return Err(ZatsuError::new("main".to_string(), error::CODE_LOADING_FILE_FAILED)),
+                };
 
-		let mut file_name = "out.dat".to_string();
-		if split.len() >= 1 {
-		    let original_file_name = split[split.len() - 1].to_string();
-		    let split: Vec<_> = original_file_name.split(".").collect();
-		    if split.len() > 1 {
-			file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
-		    }
-		}
-		match fs::write(&PathBuf::from(file_name), decoded) {
-		    Ok(()) => (),
-		    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
-		};
+                let split: Vec<_> = entry.path.split("/").collect();
+                let mut file_name = "out.dat".to_string();
+                if split.len() >= 1 {
+                    let original_file_name = split[split.len() - 1].to_string();
+                    let split: Vec<_> = original_file_name.split(".").collect();
+                    if split.len() > 1 {
+                        file_name = format!("{}-r{}.{}", split[0], self.revision_number, split[1]);
+                    }
+                }
+                match fs::write(&PathBuf::from(file_name), decoded) {
+                    Ok(()) => (),
+                    Err(_) => return Err(ZatsuError::new("main".to_string(), error::CODE_SAVING_FILE_FAILED)),
+                };
 
-		
-	    }
-	}
+                
+            }
+        }
 
-	Ok(())	
+        Ok(())  
     }
 }
