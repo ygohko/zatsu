7ce6e7083c91c000c67466faa834ee16d5663265
Yasuaki Gohko <ygohko@gmail.com>
2024-09-24 20:07:31 +0900

    Updated codes.

--- src/upgrade_command.rs
+++ src/upgrade_command.rs
@@ -3,6 +3,18 @@ struct UpgradeCommand {
 
 impl Command for UpgradeCommand {
     pub fn execute() -> Result<(), ZatsuError> {
+        let repository = match Repository::load(".zatsu") {
+            Ok(repository) => repository,
+            Err(_) => {
+                println!("Error: repository not found. To create repository, execute zatsu init.");
+                return Err(ZatsuError::new(error::CODE_LOADING_REPOSITORY_FAILED));
+            }
+        };
+        if repository.version != 1 {
+            println!("Error: Repository is already up to date. Do nothing.");
+            return Err(ZatsuError::new(error::CODE_GENERAL));
+        }
+
         // TODO: Move objects directory.
 
         // TODO: Copy objects into new new directory.
