f9ef579414cbaa5644e620be6e2845c6dfc16660
Yasuaki Gohko <ygohko@gmail.com>
2024-10-12 16:58:16 +0900

    Add tests for CommitCommand (#91)
    * Adding tests for CommitCommand.
    
    * Improved execution test.

--- src/commit_command.rs
+++ src/commit_command.rs
@@ -174,3 +174,40 @@ fn process_file(path: impl AsRef<Path>, repository_version: i32) -> Result<Strin
 
     Ok(hex_string)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::env;
+
+    use crate::InitCommand;
+
+    #[test]
+    fn is_creatable() {
+        let _command = CommitCommand::new();
+    }
+
+    #[test]
+    fn is_executable() {
+        fs::create_dir("tmp").unwrap();
+        env::set_current_dir("tmp").unwrap();
+        let command = InitCommand::new(1);
+        command.execute().unwrap();
+        let command = CommitCommand::new();
+        let result = command.execute();
+        assert!(result.is_ok());
+        env::set_current_dir("..").unwrap();
+        fs::remove_dir_all("tmp").unwrap();
+
+        fs::create_dir("tmp").unwrap();
+        env::set_current_dir("tmp").unwrap();
+        let command = InitCommand::new(2);
+        command.execute().unwrap();
+        let command = CommitCommand::new();
+        let result = command.execute();
+        assert!(result.is_ok());
+        env::set_current_dir("..").unwrap();
+        fs::remove_dir_all("tmp").unwrap();
+    }
+}
