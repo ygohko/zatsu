52a417a5814c29392a3301eec96c379feb6b66bf
Yasuaki Gohko <ygohko@gmail.com>
2025-02-05 07:56:44 +0900

    Implemented save_revision().

--- src/repository.rs
+++ src/repository.rs
@@ -33,7 +33,7 @@ use crate::Revision;
 pub trait Repository {
     fn save(&self, path: &dyn AsRef<Path>) -> Result<(), ZatsuError>;
     fn load_revision(&self, revision_number: i32) -> Result<Revision, ZatsuError>;
-    fn save_revision(&self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError>;
+    fn save_revision(&mut self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError>;
     fn revision_numbers(&self) -> Vec<i32>;
     fn set_revision_numbers(&mut self, revision_numbers: &Vec<i32>);
     fn version(&self) -> i32;
@@ -68,8 +68,32 @@ impl Repository for RepositoryBase {
         Ok(revision)
     }
 
-    fn save_revision(&self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
-        // TODO: Implement this.
+    fn save_revision(&mut self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
+        let path = format!(".zatsu/revisions/{:02x}", revision_number & 0xFF).to_string();
+        let a_path = Path::new(&path);
+        let exists = match a_path.try_exists() {
+            Ok(exists) => exists,
+            Err(_) => return Err(ZatsuError::new(error::CODE_SAVING_FILE_FAILED)),
+        };
+        if !exists {
+            match fs::create_dir(&path) {
+                Ok(()) => (),
+                Err(_) => return Err(ZatsuError::new(error::CODE_SAVING_FILE_FAILED)),
+            };
+        }
+        match revision.save(format!("{}/{}.json", &path, revision_number)) {
+            Ok(_) => (),
+            Err(_) => return Err(ZatsuError::new(error::CODE_SAVING_FILE_FAILED)),
+        };
+        let mut revision_numbers = self.revision_numbers();
+        revision_numbers.push(revision_number);
+        self.set_revision_numbers(&revision_numbers);
+        match self.save(&Path::new(".zatsu")) {
+            Ok(_) => (),
+            Err(_) => return Err(ZatsuError::new(error::CODE_SAVING_FILE_FAILED)),
+        };
+
+
         Err(ZatsuError::new(error::CODE_GENERAL))
     }
 
@@ -127,7 +151,7 @@ impl Repository for RepositoryV1 {
         self.base.load_revision(revision_number)
     }
 
-    fn save_revision(&self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
+    fn save_revision(&mut self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
         self.base.save_revision(revision, revision_number)
     }
 
@@ -169,7 +193,7 @@ impl Repository for RepositoryV2 {
         self.base.load_revision(revision_number)
     }
 
-    fn save_revision(&self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
+    fn save_revision(&mut self, revision: &Revision, revision_number: i32) -> Result<(), ZatsuError> {
         self.base.save_revision(revision, revision_number)
     }
 
